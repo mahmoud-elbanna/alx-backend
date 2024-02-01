2452cb348730d53d84d5e95d2f727633a04a36a0
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-02-01 02:56:15 +0200

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 """
-LIFOCache module
+FIFOCache module
 """
 
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """LIFOCache class"""
-
-    def __init__(self):
-        super().__init__()
-        self._last_key = None
+class FIFOCache(BaseCaching):
+    """FIFOCache class"""
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is not None and item is not None:
-            if (
-                len(self.cache_data) == self.MAX_ITEMS
-                and key not in self.cache_data.keys()
-            ):
-                discarded_key = self._last_key
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discarded_key = sorted(self.cache_data.keys())[0]
                 del self.cache_data[discarded_key]
                 print(f"DISCARD: {discarded_key}")
-
-            self.cache_data[key] = item
-            self._last_key = key
 
     def get(self, key):
         """Get an item by key"""
