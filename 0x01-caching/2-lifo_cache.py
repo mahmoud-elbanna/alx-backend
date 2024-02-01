3526716ab9de91756b326aa3060b26542e71640b
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-02-01 02:56:43 +0200

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,42 +1,32 @@
-#!/usr/bin/env python3
-""" BaseCaching module
+#!/usr/bin/python3
 """
-from base_caching import BaseCaching
+LIFOCache module
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """LIFOCache class"""
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
         super().__init__()
-        self.order = []
+        self._last_key = None
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
-        if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[-1]))
-                del self.cache_data[self.order[-1]]
-                del self.order[-1]
-            if key in self.order:
-                del self.order[self.order.index(key)]
-            self.order.append(key)
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if (
+                len(self.cache_data) == self.MAX_ITEMS
+                and key not in self.cache_data.keys()
+            ):
+                discarded_key = self._last_key
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
             self.cache_data[key] = item
+            self._last_key = key
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
