6a5ad7737aaf62e69e62a5ed33cfad62db65d793
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-02-01 02:58:57 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """
-LRUCache module
+MRUCache module
 """
 
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRUCache class"""
+class MRUCache(BaseCaching):
+    """MRUCache class"""
 
     def __init__(self):
         super().__init__()
-        self._lru_keys = []
+        self._mru_keys = []
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if key in self._lru_keys:
-                self._lru_keys.remove(key)
-            self._lru_keys.append(key)
+            if key in self._mru_keys:
+                self._mru_keys.remove(key)
+
             if len(self.cache_data) > self.MAX_ITEMS:
-                removed_key = self._lru_keys.pop(0)
+                removed_key = self._mru_keys.pop()
                 del self.cache_data[removed_key]
                 print(f"DISCARD: {removed_key}")
+            self._mru_keys.append(key)
 
     def get(self, key):
         """Get an item by key"""
         if key in self.cache_data.keys():
-            self._lru_keys.pop(self._lru_keys.index(key))
-            self._lru_keys.append(key)
+            self._mru_keys.pop(self._mru_keys.index(key))
+            self._mru_keys.append(key)
         return self.cache_data.get(key, None)
