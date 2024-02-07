3ef5edd65096d788fee12d3617002e78e76efea0
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-02-07 04:02:06 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 """
-Flask app
+0-app.py
 """
-from flask import Flask, render_template
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('0-index.html')
-
-
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+@app.route("/")
+def hello_world() -> str:
+    """Return a simple string as our response"""
+    return render_template("0-index.html")
